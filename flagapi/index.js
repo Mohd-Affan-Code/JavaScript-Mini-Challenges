3984f4d2b0bf86a2610f28d510cbbf9d236b123a
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-08-12 23:39:03 +0530

    flag project in progress

--- flagapi/index.js
+++ flagapi/index.js
@@ -1,3 +1,5 @@
+const countriesContainer = document.querySelector(".countries-container");
+
 fetch(
   "https://restcountries.com/v3.1/all?fields=name,capital,population,region,subregion,languages,currencies,flags,cca2,timezones "
 )
@@ -6,6 +8,20 @@ fetch(
   })
   .then((data) => {
     data.forEach((country) => {
-      console.log(country);
+      const countryCard = document.createElement("a");
+      countryCard.href = `/country.html?name=${country.name.common}`;
+      countryCard.classList.add("country-card");
+      countryCard.innerHTML = `
+      <img src="${country.flags.svg}" alt="flag" />
+          <div class="card-text">
+            <h3 class="card-title">${country.name.common}</h3>
+            <p><b>Population: </b>${country.population.toLocaleString(
+              "en-IN"
+            )}</p>
+            <p><b>Region: </b>${country.region}</p>
+                <p><b>Capital: </b>${country.capital?.[0]}</p> 
+          </div>
+      `;
+      countriesContainer.append(countryCard);
     });
   });
