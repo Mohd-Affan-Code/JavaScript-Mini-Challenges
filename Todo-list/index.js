3020a75d9fceb3bb200ac7853e47bfce94bebd83
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-08-13 22:41:08 +0530

    some changes in flag api

--- Todo-list/index.js
+++ Todo-list/index.js
@@ -1,13 +1,29 @@
-const input = document.querySelector("input");
-const btn = document.querySelector(".input-button");
+const input = document.getElementById("input");
+const addButton = document.querySelector(".input-button");
+const taskBox = document.querySelector(".task-box");
 
-// input.addEventListener("input", function (e) {
-//   console.log(e.target.value);
-// });
+addButton.addEventListener("click", () => {
+  const taskText = input.value.trim();
 
+  if (taskText !== "") {
+    // Create <li>
+    const li = document.createElement("li");
 
-function addTask() {
-  let task = []
+    // Add gift icon
+    const img = document.createElement("img");
+    img.src = "gift.png";
+    img.width = 20;
+    li.appendChild(img);
 
-  
-}
+    // Add task text
+    li.appendChild(document.createTextNode(" " + taskText));
+
+    // Append li to task box
+    taskBox.appendChild(li);
+
+    // Clear input
+    input.value = "";
+  } else {
+    alert("Please enter an item!");
+  }
+});
