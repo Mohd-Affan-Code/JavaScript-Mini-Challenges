65eb814f53915b801cd24e0aeae0885b836cfe29
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-08-26 14:25:42 +0530

    style add

---
+++ quizApp/Data.js
@@ -0,0 +1,79 @@
+const quizData = [
+  {
+    question: "JavaScript me default data types kaun-kaun se hote hain?",
+    options: [
+      "String, Number, Boolean, Null, Undefined, Object, Symbol",
+      "HTML, CSS, JS",
+      "Integer, Float, Double",
+      "None",
+    ],
+    answer: "String, Number, Boolean, Null, Undefined, Object, Symbol",
+  },
+  {
+    question: "HTML ka full form kya hai?",
+    options: [
+      "Hyper Text Makeup Language",
+      "Hyper Text Markup Language",
+      "High Transfer Markup Language",
+      "Hyperlinks and Text Mark Language",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question: "CSS ka use kis liye hota hai?",
+    options: [
+      "Database ke liye",
+      "Styling ke liye",
+      "Programming ke liye",
+      "Backend ke liye",
+    ],
+    answer: "Styling ke liye",
+  },
+  {
+    question: "JavaScript me array banane ka sahi syntax kya hai?",
+    options: [
+      "var arr = (1,2,3)",
+      "var arr = [1,2,3]",
+      "var arr = {1,2,3}",
+      "array(1,2,3)",
+    ],
+    answer: "var arr = [1,2,3]",
+  },
+  {
+    question: "HTML me link add karne ke liye kaunsa tag use hota hai?",
+    options: ["<p>", "<link>", "<a>", "<href>"],
+    answer: "<a>",
+  },
+  {
+    question: "CSS me text ko bold karne ke liye kaunsa property use hoti hai?",
+    options: ["font-weight", "text-bold", "font-style", "text-weight"],
+    answer: "font-weight",
+  },
+  {
+    question: "JavaScript me '===' operator kya check karta hai?",
+    options: ["Sirf value", "Sirf type", "Value aur Type dono", "None"],
+    answer: "Value aur Type dono",
+  },
+  {
+    question:
+      "HTML form me user se input lene ke liye kaunsa tag use hota hai?",
+    options: ["<input>", "<form>", "<textarea>", "<button>"],
+    answer: "<input>",
+  },
+  {
+    question:
+      "CSS me background color change karne ke liye kaunsa property use hoti hai?",
+    options: ["color", "background-color", "bg-color", "background-style"],
+    answer: "background-color",
+  },
+  {
+    question: "JavaScript me function banane ka sahi syntax kaunsa hai?",
+    options: [
+      "function = myFunc() {}",
+      "function myFunc() {}",
+      "def myFunc() {}",
+      "func myFunc() {}",
+    ],
+    answer: "function myFunc() {}",
+  },
+];
