cfed5cdc7f31e633077f6eecfa5396c9840032b1
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-08-30 23:22:22 +0530

    quiz app progress

--- quizApp/index.js
+++ quizApp/index.js
@@ -3,8 +3,11 @@ import quizData from "./Data.js";
 const question = document.querySelector(".question");
 const options = document.querySelector(".option");
 const nextbtn = document.querySelector(".next-btn");
+const quesNum = document.querySelector(".ques-num");
 
 let currentQuestion = 0;
+let questionNumber = 1;
+let score = 0;
 
 function questionLoad() {
   const currentQuiz = quizData[currentQuestion];
@@ -15,18 +18,24 @@ function questionLoad() {
     // console.log(option);
     let optionBtn = document.createElement("p");
     optionBtn.innerText = option;
-    debugger;
+    optionBtn.classList.add("option-item");
     optionBtn.onclick = () => {
-      console.log(option);
-      selectAnswere(option, currentQuiz.answer);
+      selectAnswere(optionBtn, option, currentQuiz.answer);
     };
     options.appendChild(optionBtn);
   });
 }
 
-function selectAnswere(selected, answere) {
+function selectAnswere(button, selected, answere) {
+  const allOptions = document.querySelectorAll(".option-item");
+
+  allOptions.forEach((opt) => {
+    opt.style.pointerEvents = "none"; // disables click
+    opt.style.cursor = "default"; // cursor normal ho jaye
+  });
   // console.log(selected);
   if (selected === answere) {
+    button.style.backgroundColor = "green";
     console.log("correct");
   } else {
     console.log("false");
@@ -37,8 +46,9 @@ nextbtn.addEventListener("click", () => {
   currentQuestion++;
   if (currentQuestion < quizData.length) {
     questionLoad();
+    quesNum.innerText = questionNumber += 1;
   } else {
-    nextbtn.disabled;
+    nextbtn.disabled = true;
   }
 });
 
