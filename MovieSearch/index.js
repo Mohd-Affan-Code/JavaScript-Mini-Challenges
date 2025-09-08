0056e7be13beff5988b83d42d02f52fff290fc49
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-09-08 12:36:02 +0530

    logic updated

--- MovieSearch/index.js
+++ MovieSearch/index.js
@@ -2,15 +2,42 @@
 
 // https://www.omdbapi.com/?s=endgame&page=1&apikey=2996cb1c
 
-let apiKey = "2996cb1c";
-let fetchedData = "";
-function getMovieData() {
-  fetch("https://www.omdbapi.com/?s=endgame&plot=full&apikey=2996cb1c")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      fetchedData = data;
-    });
+// let apiKey = "2996cb1c";
+
+const input = document.querySelector("input");
+const inputList = document.querySelector(".search-list");
+
+async function showMovies(movieName) {
+  let res = await fetch(
+    `https://www.omdbapi.com/?s=${movieName}&page=1&apikey=2996cb1c`
+  );
+  let data = await res.json();
+  if (data.Response == "True") displayMovieList(data.Search);
 }
 
-getMovieData();
+// search Movies in the input box
+
+input.addEventListener("input", (e) => {
+  if (input.value.length >= 2) {
+    inputList.style.display = "block";
+    console.log(e.target.value);
+  }
+});
+
+// show movies list in input dropdown
+
+function displayMovieList(moviesList) {
+  inputList.innerHTML = "";
+  moviesList.forEach((listItem) => {
+    inputList.innerHTML += `<div class="single-movie">
+            <img
+              src="${listItem.Poster}"
+              alt=""
+            />
+            <div class="movie-nameYear">
+              <p>${listItem.Title}</p>
+              <p>${listItem.Year}</p>
+            </div>
+      </div>`;
+  });
+}
