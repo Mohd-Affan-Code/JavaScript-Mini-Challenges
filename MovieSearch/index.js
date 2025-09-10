31cedae8c99dcbe0c242a39b2987f4a6d354d7db
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-09-10 10:54:24 +0530

    and new updated file

--- MovieSearch/index.js
+++ MovieSearch/index.js
@@ -6,6 +6,7 @@
 
 const input = document.querySelector("input");
 const inputList = document.querySelector(".search-list");
+const detailContainer = document.querySelector(".detail-container");
 
 async function showMovies(movieName) {
   let res = await fetch(
@@ -17,10 +18,14 @@ async function showMovies(movieName) {
 
 // search Movies in the input box
 
+let debounceTimer;
 input.addEventListener("input", (e) => {
-  if (input.value.length >= 2) {
-    inputList.style.display = "block";
-    console.log(e.target.value);
+  clearTimeout(debounceTimer);
+  if (input.value.trim().length >= 2) {
+    debounceTimer = setTimeout(() => {
+      inputList.style.display = "block";
+      showMovies(e.target.value.trim());
+    }, 500);
   }
 });
 
@@ -28,16 +33,69 @@ input.addEventListener("input", (e) => {
 
 function displayMovieList(moviesList) {
   inputList.innerHTML = "";
+  console.log(moviesList);
   moviesList.forEach((listItem) => {
-    inputList.innerHTML += `<div class="single-movie">
-            <img
-              src="${listItem.Poster}"
-              alt=""
-            />
-            <div class="movie-nameYear">
-              <p>${listItem.Title}</p>
-              <p>${listItem.Year}</p>
-            </div>
-      </div>`;
+    let poster = listItem.Poster !== "N/A" ? listItem.Poster : "default.jpg";
+    let movieDiv = document.createElement("div");
+    movieDiv.classList.add("single-movie");
+
+    movieDiv.innerHTML = `
+      <img src="${poster}" alt="" />
+      <div class="movie-nameYear">
+        <p>${listItem.Title}</p>
+        <p>${listItem.Year}</p>
+      </div>
+    `;
+    movieDiv.addEventListener("click", () => {
+      showMoviesDetails(listItem.Title, listItem.Year);
+      inputList.style.display = "none";
+    });
+    inputList.appendChild(movieDiv);
   });
+}
+
+async function showMoviesDetails(movieName, year) {
+  let res = await fetch(
+    `https://www.omdbapi.com/?t=${movieName}&y=${year}&apikey=2996cb1c`
+  );
+  let movieData = await res.json();
+  console.log(movieData);
+  detailContainer.innerHTML = "";
+  let firstEl = document.createElement("div");
+  let secondEl = document.createElement("div");
+
+  firstEl.classList.add("right");
+  secondEl.classList.add("left");
+
+  firstEl.innerHTML = `
+  <div class="poster">
+            <img
+              src="${movieData.Poster}"
+              alt="movie Poster"
+            />
+            <p class="runtime"><b>Runtime:</b> ${movieData.Runtime}</p>
+          </div>
+  `;
+  secondEl.innerHTML = `
+  <div class="movie-name">
+            <p>${movieData.Title}</p>
+          </div>
+          <div class="movie-dates">
+            <p class="year"><b> Year:</b> ${movieData.Year}</p>
+            <p class="rating"><b> Rating:</b> ${movieData.Rated}</p>
+            <p class="released"><b> Released:</b> ${movieData.Released}</p>
+          </div>
+          <p class="genre"><b>Genre:</b> ${movieData.Genre}</p>
+          <p class="writer">
+            <b>Writer:</b> ${movieData.Writer}
+          </p>
+          <p class="actors">
+            <b>Actors:</b> ${movieData.Actors}
+          </p>
+          <p class="plot">
+            <b>Plot:</b> ${movieData.Plot}
+          </p>
+          <p class="language"><b>Language:</b> ${movieData.Language}</p>
+  `;
+  detailContainer.append(firstEl, secondEl);
 }
